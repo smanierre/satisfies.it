48a918e29a298ee4b5d4a5c4745cd0fcc620ee95
Sean Manierre <sean.manierre@gmail.com>
2020-09-23 07:56:53 -0400

    Updated IsGoFileUtil to check length of the file before assuming the filename was 3 characters or greater

--- backend/util/isGoFile.go
+++ backend/util/isGoFile.go
@@ -2,6 +2,9 @@ package util
 
 //IsGoFile checks if a file is a go file by looking for the file to end with .go
 func IsGoFile(filename string) bool {
+	if len(filename) < 3 {
+		return false
+	}
 	lastThree := filename[len(filename)-3:]
 	if lastThree != ".go" {
 		return false
