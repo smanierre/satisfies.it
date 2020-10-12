4eda94cc3612d54ff27bb75e55e4cbf029428f50
Sean Manierre <sean.manierre@gmail.com>
2020-10-11 23:19:03 -0400

    Updated list page to make it not look as bad.

--- frontend/src/InterfaceResults/Result.tsx
+++ frontend/src/InterfaceResults/Result.tsx
@@ -6,8 +6,12 @@ import { ICustomTypeResult } from "../types";
 
 const ResultContainer = styled(Link)`
   width: 100%;
+  height: 50px;
+  line-height: 50px; ${/*Just to center the text vertically*/""}
+  flex-shrink: 0;
   color: #f8f8f8;
   padding: 3px;
+  padding-left: 15px;
   font-size: 1.6rem;
 `;
 
