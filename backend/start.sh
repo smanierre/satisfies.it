890ec10ec1bc8b787bc448a5e774c45a40b177cb
Sean Manierre <sean.manierre@gmail.com>
2020-10-14 11:17:01 -0400

    Explicitly stated the location of the types.json file in the docker file. Also updated the path for the mounted volume to be correct in the start script.

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -v .etc/letsencrypt/live/satisfies.it:/certs smanierre/typer-site --name typersite
+docker run -v /etc/letsencrypt/live/satisfies.it:/certs smanierre/typer-site --name typersite
