8633f6a99f8bdd11456955460860b64acf8e754b
Sean Manierre <sean.manierre@gmail.com>
2020-10-24 19:09:16 -0400

    Don't even know anymore.

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,10 +1,9 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"regexp"
+	"strings"
 
 	"gitlab.com/sean.manierre/typer-site/store"
 )
@@ -38,23 +37,12 @@ func (s Server) registerEndpoints() {
 
 //getRoot handles serving the files for the React SPA frontend and redirects and non-api calls to the home page so the app can load.
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	// if r.URL.Path != "/" && !strings.Contains(r.URL.Path, "/static") && r.URL.Path != "/logo.svg" {
-	// 	http.Redirect(w, r, "/", http.StatusFound)
-	// 	return
-	// }
-	// fs := http.FileServer(http.Dir("./static/"))
-	// fs.ServeHTTP(w, r)
-	fileServer := http.FileServer(http.Dir("static"))
-	fileMatcher := regexp.MustCompile(`\.[a-zA-Z]*$`)
-	if !fileMatcher.MatchString(r.URL.Path) {
-		http.ServeFile(w, r, "static/index.html")
-	} else {
-		if r.URL.Path[len(r.URL.Path)-3:] == ".js" {
-			w.Header().Set("content-type", "application/javascript")
-			fmt.Println("Writing javascript header")
-		}
-		fileServer.ServeHTTP(w, r)
+	if r.URL.Path != "/" && !strings.Contains(r.URL.Path, "/static") && r.URL.Path != "/logo.svg" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
 	}
+	fs := http.FileServer(http.Dir("./static/"))
+	fs.ServeHTTP(w, r)
 }
 
 //AllowCorsMiddleware allows CORS requests from any source for development purposes only.
